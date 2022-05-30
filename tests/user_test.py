1493f4db714984f58d4f613212281b94442f07c7
BizTecBritain <BizTecBritain@gmail.com>
2022-05-30 12:51:36 +0100

    Add files via upload

---
+++ tests/user_test.py
@@ -0,0 +1,13 @@
+from src.KMap import KarnaughMap
+
+
+def run():
+    exp = input()
+    kmap = KarnaughMap(expression=exp)
+    kmap.create_map()
+    # kmap.print()
+    # print(kmap.to_string())
+    # print(str(kmap))
+    solution = kmap.solve_map()
+    # print(solution)
+    return exp, solution, str(kmap)
