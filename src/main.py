5adc173a311b3cc8eadc85f405b932c549c90e05
BizTecBritain <BizTecBritain@gmail.com>
2022-05-30 12:50:39 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from src.KMap import GUI
+from tkinter import Tk
+
+
+if __name__ == "__main__":
+    # tests.run()
+    # tests.fixed_test_4.run()
+    root = Tk()
+    app = GUI.App(root)
+    root.mainloop()
